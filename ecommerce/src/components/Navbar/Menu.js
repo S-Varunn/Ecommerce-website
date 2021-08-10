34d05a053adc90af0d79aa41662860a93995f2fc
S-Varunn <85671418+S-Varunn@users.noreply.github.com>
2021-08-10 10:42:04 +0530

    Navbar layout and design

---
+++ ecommerce/src/components/Navbar/Menu.js
@@ -0,0 +1,32 @@
+export const Menu = [
+  {
+    label: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    label: "Products",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    label: "Todays Deals",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    label: "Contact Us",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    label: "View Cart",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    label: "Sign Up",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
--- ecommerce/src/components/Navbar/navbar.js
+++ ecommerce/src/components/Navbar/navbar.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
+import { Menu } from "./Menu";
+import "./navbar.css";
 class Navbar extends Component {
-  state = {};
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
   render() {
-    return <nav className="Nbar"></nav>;
+    return (
+      <nav className="Nbar">
+        <h1 className="logo">Bubbles</h1>
+        <i class="fas fa-biohazard"></i>
+
+        <div className="menuIcon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {Menu.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.label}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
   }
 }
 
